9899437cfad87815f33f95be9b2105771601c193
raffaddtg@gmail.com <raffaddtg@gmail.com>
2022-05-01 23:05:54 -0300

    ...

--- js/index.js
+++ js/index.js
@@ -1,52 +1,100 @@
 const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
 
-
-var fireDataStructure = []
-var width = 80
-var height = 80
-var pixelSize = 0
-var colorMode = "1"
-var windDirection = 1
+//Fire data:
+var fireDataStructure = [] //pixels array
+var width = 80 //width of the pixel matrix
+var height = 80 //height of the pixel matrix
+var pixelSize = 0 //size of the pixels in the canvas (auto)
+var colorMode = "1" //color effect mode, used to apply color effects
+var windDirection = 1 //direction of the fire, can be 0 (left), 1 (up), or 2 (right)
 
 var canvas = document.createElement('canvas')
-var context = canvas.getContext("2d")
-document.querySelector('#fireCanvas').appendChild(canvas)
 canvas.width = 300
 canvas.height = 300
-pixelSize = canvas.width/(width-1)
-canvas.addEventListener('touchmove', function(e) {
-    var rect = canvas.getBoundingClientRect()
-    var touchPos = {
-        x: Math.floor((e.touches[0].clientX - rect.left) / pixelSize),
-        y: Math.floor((e.touches[0].clientY - rect.top) / pixelSize)
-    }
-    draw(touchPos)
-}, true)
-canvas.addEventListener('mousemove', function(e) {
-    var rect = canvas.getBoundingClientRect()
-    var mousePos = {
-        x: Math.floor((e.clientX - rect.left) / pixelSize),
-        y: Math.floor((e.clientY - rect.top) / pixelSize)
-    }
-    draw(mousePos)
-}, true)
+var context = canvas.getContext("2d")
+document.querySelector('#fireCanvas').appendChild(canvas)
+
 var selectColorMode = document.getElementById('selectColorMode')
 selectColorMode.onchange = function() {
     colorMode = selectColorMode.options[selectColorMode.options.selectedIndex].value
 }
-var windDirectionSlider = document.getElementById('windDirection') 
+var windDirectionSlider = document.getElementById('windDirection')
 windDirectionSlider.oninput = function() {
     windDirection = parseInt(this.value)
 }
+var canvasInputListener = createInputListener(canvas, ['mousemove', 'touchmove'])
+mouseInput = {
+    update: function(command) {
+        var element = command.element
+        var whitch = command.whitch
+        var event = command.event
+        if (whitch === 'mousemove') {
+            var rect = element.getBoundingClientRect()
+            var mousePos = {
+                x: Math.floor((event.clientX - rect.left) / pixelSize),
+                y: Math.floor((event.clientY - rect.top) / pixelSize)
+            }
+            draw(mousePos)
+        }
+    }
+}
+var touchInput = {
+    update: function(command) {
+        var element = command.element
+        var whitch = command.whitch
+        var event = command.event
+        if (whitch === 'touchmove') {
+            var rect = element.getBoundingClientRect()
+            var touchPos = {
+                x: Math.floor((event.touches[0].clientX - rect.left) / pixelSize),
+                y: Math.floor((event.touches[0].clientY - rect.top) / pixelSize)
+            }
+            draw(touchPos)
+        }
+    }
+}
+canvasInputListener.subscribe(mouseInput)
+canvasInputListener.subscribe(touchInput)
+
 createFireDataStructure(width, height)
 createFireSource()
-setInterval(calculateFirePropagation, )
+setInterval(calculateFirePropagation, 60)
+
+function createInputListener(listerningObject, callbackNames) {
+    var inputListener = createObserverPattern()
+    for (let callback in callbackNames) {
+        listerningObject.addEventListener(callbackNames[callback], function(e) {
+            inputListener.notifyAll({ element: listerningObject, whitch: callbackNames[callback], event: e })
+        }, true)
+    }
+    return inputListener
+}
+
+function createObserverPattern() {
+    var subject = {}
+    subject.observers = []
+    subject.subscribe = function(observer) {
+        this.observers.push(observer)
+    }
+    subject.unsubscribe = function(observer) {
+        this.observers.splice(observer)
+    }
+    subject.notifyAll = function(command) {
+        for (let observer in this.observers) {
+            this.observers[observer].update(command)
+        }
+    }
+    return subject
+}
 
 function draw(pos) {
+    pixelSize = canvas.width/(width-1)
     for (let x = -2; x < 2; x++) {
         for (let y = -2; y < 2; y++) {
             var center = (pos.y * width + pos.x)
-            fireDataStructure[center + (y + Math.floor(-1.5 + Math.random() * 3)) * width + x] = Math.floor(21+Math.random()*15);
+            var drawPos = center + (y + Math.floor(-1.5 + Math.random() * 3)) * width + x
+            if (drawPos >= width * (height-1)) continue
+            fireDataStructure[drawPos] = Math.floor(21+Math.random()*15);
         }
     }
 }
@@ -106,6 +154,7 @@ function calculateFirePropagation() {
 }
 
 function renderFire() {
+    pixelSize = canvas.width/width
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
             var color = fireColorsPalette[fireDataStructure[y*width+x]]
@@ -137,4 +186,3 @@ function renderFire() {
         }
     }
 }
-
